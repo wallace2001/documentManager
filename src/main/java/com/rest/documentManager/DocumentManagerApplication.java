d6b4491fba036d80193e5ea20b5798c44091755b
wallace <kollen22@outlook.com>
2022-04-15 10:38:43 -0300

    first commit

---
+++ src/main/java/com/rest/documentManager/DocumentManagerApplication.java
@@ -0,0 +1,13 @@
+package com.rest.documentManager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DocumentManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DocumentManagerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rest/documentManager/DocumentManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rest.documentManager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DocumentManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
